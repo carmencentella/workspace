553d5e4800b3ff696d2bf20f619f444f6469c775
carmencentella <carcenjar@alu.edu.gva.es>
2024-11-06 13:54:36 +0100

    listas2

--- ejerciciosListas/Listas4.py
+++ ejerciciosListas/Listas4.py
@@ -1,2 +1,16 @@
-top=int(input('Dígame un número: '))
-for i in ()
+n=int(input('Dígame un número: '))
+list=[]
+for i in range (1,n+1):
+   primo=True
+   for j in range(2,i):
+        if i == j:
+           break
+        elif i%j == 0:
+           primo = False
+        else:
+           continue
+   if primo == True:
+      list.append(i)
+print(f'Primos hasta {n}: {list}')
+
+
