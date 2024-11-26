6ee68c39beb8ab212940d6a04cb1e6ccd720d528
carmencentella <carcenjar@alu.edu.gva.es>
2024-11-26 13:55:21 +0100

    VISUALIZACIONDATOS

---
+++ ejerciciosFicheros/ProyectoCombinarCorrespondencia_Inicial/main.py
@@ -0,0 +1,8 @@
+#TODO: Crea una carta usando carta_inicial.txt 
+#para cada nombre en nombres_invitados.txt
+#Cambia el marcador [nombre] con el nombre real.
+#Guarda las cartas en la carpeta "ListosParaEnviar".
+    
+#Pista1: Este método te ayudará: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Pista2: Este método también te ayudará: https://www.w3schools.com/python/ref_string_replace.asp
+        #Pista3: Este método te ayudará: https://www.w3schools.com/python/ref_string_strip.asp
