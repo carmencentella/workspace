0c7a67e448fe953f489ffbdd2d7a7c0e8935abb7
carmencentella <carcenjar@alu.edu.gva.es>
2024-12-12 13:56:47 +0100

    cafe

--- EjemplosPOO/Coffee machine project/oop-coffee-machine-start/main2.py
+++ EjemplosPOO/Coffee machine project/oop-coffee-machine-start/main2.py
@@ -2,6 +2,21 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+import streamlit as st
+
+st.title("Máquina de café")
+st.write("Disfruta de un café hoy")
+
+
+# STEP5: Organizamos las entradas y el botón en columnas para evitar desplazamiento vertical
+# Ej.: Castellon --> lat = 39.98567, long=-0.04935
+col1, col2 = st.columns([1, 1])
+with col1:
+    choice = st.selectbox('Introduce tu selección', ('expresso', 'late', 'capuccino'))
+with col2:
+    lon = st.number_input("Introduce el dinero:", value=0.0, format="%.2f", step=0.02)
+
+fetch_button = st.button("Obtén tu café")
 
 money_machine=MoneyMachine()
 coffee_maker=CoffeeMaker() 
